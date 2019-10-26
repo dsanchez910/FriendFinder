b4013157b6519b2effe25f1cbeca6f470f0caa23
Danny Sanchez <dsanchez910@gmail.com>
2019-10-26 10:08:03 -0400

    added friends match objects and pictures

---
+++ app/data/friends.js
@@ -0,0 +1,24 @@
+var friendMatch = [
+    {
+      name: "Kanye West",
+      photo: "https://ksassets.timeincuk.net/wp/uploads/sites/55/2019/09/Kanye-West-.jpg",
+      scores: ["3", "3", "3", "3", "3"]
+    }, {
+      name: "Oprah Winfrey",
+      photo: "https://a9p9n2x2.stackpathcdn.com/wp-content/blogs.dir/1/files/2019/09/oprah-2020-1080x675.jpg",
+      scores: ["2", "2", "2", "2", "2"]
+    }, {
+      name: " Derek Jeter",
+      photo: "https://cdn.newsday.com/polopoly_fs/1.34072344.1563663574!/httpImage/image.jpg_gen/derivatives/landscape_768/image.jpg",
+      scores: ["1", "1", "1", "1", "1"]
+    }, {
+      name: "Christen Press",
+      photo: "https://upload.wikimedia.org/wikipedia/commons/8/8d/President_Barack_Obama.jpg",
+      scores: ["2", "3", "2", "3", "1"]
+    },
+  
+  ];
+  
+  
+  
+  module.exports = friendMatch;
